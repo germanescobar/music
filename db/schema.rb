bdf1f381f4b236d30f1f35ff8110bdbd7557491f
German Escobar <german.escobarc@gmail.com>
2015-05-15 22:36:51 -0500

    Create model Album

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150516033345) do
+ActiveRecord::Schema.define(version: 20150516033630) do
+
+  create_table "albums", force: :cascade do |t|
+    t.string   "name"
+    t.string   "image_url"
+    t.date     "release_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "artists", force: :cascade do |t|
     t.string   "name"
